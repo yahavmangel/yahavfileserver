02f7eedc5ab4dc32fba71c6df165a4ea0854f4de
yahavmangel <yahavmangel@gmail.com>
2024-08-27 22:56:13 -0500

    added support for all binary files and added overwriting detection

--- socket_programming/client_code/client.py
+++ socket_programming/client_code/client.py
@@ -1,24 +1,36 @@
 import socket
 
-def send_text_file(filename, server_ip):
+def server_request(filename, server_ip, command):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, 65432))
+    client_socket.connect((server_ip, 12347))
 
-    with open(filename, "w") as file:
-        file.write("Please receive this \n")
-    with open(filename, 'rb') as file:
-        while True:
-            data = file.read(1024)
-            if not data:
-                break
-            client_socket.send(data)
-    print("File sent successfully")
+    server_msg = f"{command}|{filename}"
+    client_socket.sendall(server_msg.encode()) # send server message 
+    server_resp = client_socket.recv(1024).decode('utf-8') # collect initial response from server 
 
+    if server_resp == 'OVERWRITE': # detect overwriting
+        user_input = input("File already exists. Overwrite? [y/n] ") # prompt user on action
+        if user_input == 'n':
+            print("Request cancelled.")
+            server_msg = "QUIT" # notify server that you quit
+            client_socket.sendall(server_msg.encode()) 
+            client_socket.close() # close connection
+        elif user_input == 'y':
+            server_msg = "ACK" # notify server that you acknowledge overwrite
+            client_socket.sendall(server_msg.encode())
+    
+    if server_resp == 'READY': # server indicates operation is ready 
+        if command == 'STORE':
+            with open(filename, 'rb') as file:
+                data = file.read()  # Read the whole file
+                client_socket.sendall(data)  # Send all the data
+                print("File sent successfully")
+        elif command == 'REQUEST':
+            print("Server requests not implemented yet")
+    
+    if server_msg == 'SERVFAIL': # case of no authorization? 
+        print("Error sending file")
     client_socket.close()
 
-# def send_pdf_file(filename, server_ip):
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client_socket.connect((server_ip, 65432))
-
 if __name__ == "__main__":
-    send_text_file('test_file.txt', '192.168.1.224')
+    server_request('ye.png', '192.168.1.224', 'STORE')
--- socket_programming/server_code/server.py
+++ socket_programming/server_code/server.py
@@ -2,25 +2,47 @@ import socket
 import os 
 
 def start_server():
+    cancel_flag = 0
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 65432))  # Bind to all interfaces on port 65432
+    server_socket.bind(('0.0.0.0', 12347))  # Bind to all interfaces on port
     server_socket.listen(1)
     print("Server is listening...")
 
-    conn, addr = server_socket.accept() #addr will contain tuple with (ip addr, port)
+    conn, addr = server_socket.accept() # addr will contain tuple with (ip addr, port)
     print(f"Connected by {addr}")
 
-    hostname, alias, ip_addresses = socket.gethostbyaddr(addr[0]) #reverse DNS lookup on ip address to get hostname
-    
+    hostname, alias, ip_addresses = socket.gethostbyaddr(addr[0]) # reverse DNS lookup on ip address to get hostname
+    client_msg = conn.recv(1024).decode('utf-8') # receive and parse initial client message
+    command, filename = client_msg.split('|', 1)
     client_dir = hostname.replace('.yahavfileserver.home', '', 1) #make new directory in file server with hostname as name  
-    os.makedirs(client_dir, exist_ok=True)
-    with open(os.path.join(client_dir, "received_file.txt"), 'wb') as file: #join file name with newly made directory 
-        while True:
-            data = conn.recv(1024)
-            if not data:
+    if os.path.exists(os.path.join(client_dir, filename)) and command == 'STORE': # detect case of overwrite
+        conn.sendall(b'OVERWRITE') # notify client of potential overwrite
+        client_msg = conn.recv(1024).decode('utf-8') # receive client response to overwrite
+        while True: 
+            if ((client_msg == 'ACK') or (client_msg == 'QUIT')): # wait for client response to process
                 break
-            file.write(data)
-    print("File received successfully")
+    if client_msg == 'QUIT': # client decided to abort to avoid overwrite
+        cancel_flag = 1
+        print("Request cancelled.")
+        conn.close()
+    if not cancel_flag: 
+        conn.sendall(b'READY') # eventually, do some authentication before this. But for now, always indicate ready. 
+        if command == 'STORE':
+            os.makedirs(client_dir, exist_ok=True)
+            with open(os.path.join(client_dir, filename), 'wb') as file: # join file name with newly made directory 
+                data_flag = 0
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        if not data_flag:
+                            os.remove(os.path.join(client_dir, filename)) # if file is empty, don't actually create anything
+                            print("Error: file contained no data")   
+                        break
+                    file.write(data)
+                    data_flag = 1
+                if data_flag: print("File stored successfully")
+        elif command == 'REQUEST':
+            print("Server requests not implemented yet")
 
     conn.close()
 
