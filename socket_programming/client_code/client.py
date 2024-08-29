461574de0c23834da6f7275fa362ff0211550616
yahavmangel <yahavmangel@gmail.com>
2024-08-29 17:05:27 -0500

    added basic file requests

--- socket_programming/client_code/client.py
+++ socket_programming/client_code/client.py
@@ -1,49 +1,112 @@
 import socket
 import sys
+import json
+import os
 
 def server_request(server_ip, command, filename):
+
+    # input validation
+    
+    if not (command == 'STORE' or command == 'REQUEST'):
+        print("Error: invalid command")
+        return 
+    
+    # connection setup
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client_socket.connect((server_ip, 12346))
     server_msg = f"{command}|{filename}"
     client_socket.sendall(server_msg.encode()) # send server message 
-    server_resp = client_socket.recv(1024).decode('utf-8') # collect initial response from server 
+
+    # handle initial server response
+
     
-    while True: 
-        if (server_resp == 'OVERWRITE') or (server_resp == 'READY'): 
-            break
-    if server_resp == 'OVERWRITE': # detect overwriting
-        print(server_resp)
-        while True:
-            user_input = input("File already exists. Overwrite? [y/n] ") # prompt user on action
-            if user_input == 'n' or user_input == 'y':
-                break
-            else:
-                print ("Invalid input, try again")
-        if user_input == 'n':
-            print("Request cancelled.")
-            server_msg = "QUIT" # notify server that you quit
-            client_socket.sendall(server_msg.encode()) 
-            client_socket.close() # close connection
-        elif user_input == 'y':
-            server_msg = "ACK" # notify server that you acknowledge overwrite
-            client_socket.sendall(server_msg.encode())
-    if not server_msg == "QUIT": 
+    
+    if command == 'STORE':
+
         while True: 
-            server_resp = client_socket.recv(1024).decode('utf-8')
-            if (server_resp == 'READY'): 
+            server_resp = client_socket.recv(1024).decode('utf-8') # collect initial response from server 
+            if (server_resp == 'OVERWRITE') or (server_resp == 'READY'): 
                 break
+        
+        # handle file overwriting case
+
+        if server_resp == 'OVERWRITE': # detect overwriting
+            while True:
+                user_input = input("File already exists. Overwrite? [y/n] ") # prompt user on action
+                if user_input == 'n' or user_input == 'y':
+                    break
+                else:
+                    print ("Invalid input, try again")
+            if user_input == 'n':
+                print("Request canceled.")
+                server_msg = "QUIT" # notify server that you quit
+                client_socket.sendall(server_msg.encode()) 
+                client_socket.close() # close connection
+            elif user_input == 'y':
+                server_msg = "ACK" # notify server that you acknowledge overwrite
+                client_socket.sendall(server_msg.encode())
+        if not server_msg == "QUIT": 
+            while True: 
+                server_resp = client_socket.recv(1024).decode('utf-8')
+                if (server_resp == 'READY'): 
+                    break
+
+    # handle server response to request 
+
+    if command == 'REQUEST':
+        while True:
+            server_resp = client_socket.recv(1024).decode('utf-8')
+            if server_resp == 'READY':
+                break
+
     if server_resp == 'READY': # server indicates operation is ready 
-        print(server_resp)
+
         if command == 'STORE':
             with open(filename, 'rb') as file:
                 data = file.read()  # Read the whole file
                 client_socket.sendall(data)  # Send all the data
                 print("File sent successfully")
                 client_socket.close()
+
         elif command == 'REQUEST':
-            print("Server requests not implemented yet")
-    
+        
+            json_list = client_socket.recv(4096).decode('utf-8')
+            options = json.loads(json_list)
+
+            if (len(options) > 1): 
+                print("Server returned multiple results: \n")
+                i = 0
+                for option in options: 
+                    i += 1
+                    print (str(i) + ': ' + option)
+                print (str(i + 1) + ": N/A \n")
+                while True:
+                    user_input = input("Which one? ")
+                    if(int(user_input) > 0 and int(user_input) < len(options) + 2):
+                        break
+                    else: 
+                        print("Invalid input, try again\n")
+                server_msg = user_input
+                client_socket.sendall(server_msg.encode()) # send server message 
+                if int(user_input) > len(options):
+                    print("Sorry we couldn't find your file :(")
+                    client_socket.close()
+                else: 
+                    with open(os.path.join('client-code/', (options[int(user_input)-1]).replace('/', '_', 1)), 'wb') as file: 
+                        while True: 
+                            data = client_socket.recv(1024)
+                            if not data:
+                                break
+                            file.write(data)
+                        print("File received successfully")
+                        client_socket.close()
+
+            # handle server response: either SERVFAIL and quit, or READY and send ACK back.
+            # store file in local storage
+            # close connection
+
     if server_msg == 'SERVFAIL': # case of no authorization? 
         print("Error sending file")
         client_socket.close()
--- socket_programming/server_code/server.py
+++ socket_programming/server_code/server.py
@@ -1,38 +1,51 @@
 import socket
 import os 
 import sys
+import json
+
+from difflib import get_close_matches
 
 def start_server():
-    cancel_flag = 0
+
+    # connection setup
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('0.0.0.0', 12346))  # Bind to all interfaces on port
     server_socket.listen(1)
     print("Server is listening...")
-
     conn, addr = server_socket.accept() # addr will contain tuple with (ip addr, port)
     print(f"Connected by {addr}")
+
+    # collect connection information
 
     hostname, alias, ip_addresses = socket.gethostbyaddr(addr[0]) # reverse DNS lookup on ip address to get hostname
     client_msg = conn.recv(1024).decode('utf-8') # receive and parse initial client message
     command, filename = client_msg.split('|', 1)
-    client_dir = hostname.replace('.yahavfileserver.home', '', 1) #make new directory in file server with hostname as name  
+    client_dir = hostname.replace('.yahavfileserver.home', '', 1) # get client's directory name in fileserver 
+
+    # check and handle file overwrite case
+
     if os.path.exists(os.path.join(client_dir, os.path.basename(filename))) and command == 'STORE': # detect case of overwrite
         conn.sendall(b'OVERWRITE') # notify client of potential overwrite
         client_msg = conn.recv(1024).decode('utf-8') # receive client response to overwrite
-        print(client_msg)
         while True: 
-            if ((client_msg == 'ACK') or (client_msg == 'QUIT')): # wait for client response to process
+            if ((client_msg == 'ACK') or (client_msg == 'QUIT')): # wait for client response to overwrite
                 break
+    cancel_flag = 0
     if client_msg == 'QUIT': # client decided to abort to avoid overwrite
         cancel_flag = 1
-        print("Request cancelled.")
+        print("Request canceled.")
         conn.close()
         server_socket.close()
+    
+    # handle main requests
+
     if not cancel_flag: 
-        conn.sendall(b'READY') # eventually, do some authentication before this. But for now, always indicate ready. 
+        
         if command == 'STORE':
-            os.makedirs(client_dir, exist_ok=True)
+            conn.sendall(b'READY') # eventually, do some authentication before this. But for now, always indicate ready. 
+            os.makedirs(client_dir, exist_ok=True) # make directory for new host (or just don't do anything if already exists)
             with open(os.path.join(client_dir, os.path.basename(filename)), 'wb') as file: # join file name with newly made directory 
                 data_flag = 0
                 while True:
@@ -48,7 +61,35 @@ def start_server():
                     print("File stored successfully")
                     
         elif command == 'REQUEST':
-            print("Server requests not implemented yet")
+            conn.sendall(b'READY')
+            json_list = json.dumps(similarity_search("../server-code", filename)) # call search algorithm and send result to client
+            if not json.loads(json_list): 
+                print("Error: search unsuccessful. Closing.")
+            else:     
+                conn.sendall(json_list.encode('utf-8'))
+
+                client_msg = conn.recv(1024).decode('utf-8')
+                options = json.loads(json_list) 
+                if int(client_msg) > len(options):
+                    print("Error: search unsuccessful. Closing.")
+                else: 
+                    target_file = options[int(client_msg)-1]
+                    with open(target_file, 'rb') as file: 
+                        data = file.read()
+                        conn.sendall(data)
+                        print("File sent successfully")
+                
+
+
+
+            # check if file exists in fileserver 
+            # if it doesn't, return an error status to client (SERVFAIL?)
+            # if it does, gather bookkeeping info about file (and later, check if client is authorized to access it, if they are not return SERVFAIL)
+            # if all is well, send READY signal to client
+            # receive ACK signal from client
+            # send file to client
+
+    # close connection
 
     conn.close()
     server_socket.close()
@@ -58,6 +99,17 @@ def test_dir_scan(dir_name):
         print(f"root: {root}")
         print(f"dirs: {dirs}")
         print(f"files: {files}")
+
+def similarity_search(dir_name, keyword):
+    file_dict = {}
+    for root, dirs, files in os.walk(dir_name):
+        for file in files: 
+            filepath = os.path.join(root.replace('../server-code/', '', 1), file)
+            file_dict.update({filepath:os.path.basename(filepath)})
+    matches = get_close_matches(keyword, list(file_dict.values()), n=10, cutoff=0.5)
+    return [match for match, base_name in file_dict.items() if base_name in matches]
+        
 if __name__ == "__main__":
     start_server()
     # test_dir_scan(sys.argv[1])
+    # similarity_search('../server-code', 'receive')
